d6f5bd2c34753885a6b1f01907660d754134a993
MADELab Dev <baonguyenyam@gmail.com>
2024-04-13 09:17:25 -0500

    qădqw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/LandingPage';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import Notification from './Components/Notification/Notification';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Navbar/>
+            <Notification>
+
+              <Routes>
+                <Route path="/" element={<Landing_Page/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/signup" element={<Sign_Up/>}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
+                <Route path="<component_route>" element={<component_name/>}/> 
+              </Routes>
+            </Notification>
+        </BrowserRouter>
     </div>
   );
 }
-
 export default App;
